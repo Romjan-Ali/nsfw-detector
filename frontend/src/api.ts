d788850318162bc34d617b5117311ffc86b54a80
Romjan Ali <000romjanali@gmail.com>
2025-09-09 23:57:58 +0600

    refactor api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,11 +1,13 @@
 // api.js
 import axios from 'axios'
 
+console.log('All env vars:', import.meta.env)
+
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_NODE_ENV === 'development'
       ? 'http://localhost:5000/api' // Localhost in development
-      : 'https://nsfw-detector-93nm.onrender.com/api', // Same domain in production
+      : '/api', // Same domain in production
   withCredentials: true,
 })
 
