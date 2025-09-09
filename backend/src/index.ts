dbd5f633b225a4e814b6a27016574b7b5d5718fb
Romjan Ali <000romjanali@gmail.com>
2025-09-09 19:55:50 +0600

    refactor package.json

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,35 @@
-import express from "express";
-import fetch from "node-fetch";
-import cors from "cors";
-import router from "./routes/upload";
+import express from 'express'
+import path from 'path'
+import fetch from 'node-fetch'
+import cors from 'cors'
+import router from './routes/upload'
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use('/api',router)
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use('/api', router)
 
-app.get("/proxy", async (req, res) => {
-  const { url } = req.query;
-  if (!url || typeof url !== "string") {
-    return res.status(400).json({ error: "Missing URL" });
+app.get('/proxy', async (req, res) => {
+  const { url } = req.query
+  if (!url || typeof url !== 'string') {
+    return res.status(400).json({ error: 'Missing URL' })
   }
 
   try {
-    const response = await fetch(url);
-    const buffer = await response.arrayBuffer();
+    const response = await fetch(url)
+    const buffer = await response.arrayBuffer()
 
-    res.setHeader("Content-Type", response.headers.get("content-type") || "image/jpeg");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.send(Buffer.from(buffer));
+    res.setHeader(
+      'Content-Type',
+      response.headers.get('content-type') || 'image/jpeg'
+    )
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.send(Buffer.from(buffer))
   } catch (err) {
-    res.status(500).json({ error: "Failed to fetch image" });
+    res.status(500).json({ error: 'Failed to fetch image' })
   }
-});
+})
 
-app.listen(5000, () => console.log("Proxy server running on port 5000"));
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+app.listen(5000, () => console.log('Proxy server running on port 5000'))
